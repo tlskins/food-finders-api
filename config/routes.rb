bde0594585c07cccd91412667ec987ba60888165
Tim Lee <tim@tryhungry.com>
2018-01-09 19:32:50 -0500

    initial vote - vote total - user - food - entity architecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :votes
+  resources :users
   resources :entities
   resources :foods
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
