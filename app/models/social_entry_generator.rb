0921b16f57d25f4fd7b04ffd07d773cdd6faeef3
Tim Lee <tlee87@gmail.com>
2018-03-26 00:22:41 -0400

    clean up comments

--- app/models/social_entry_generator.rb
+++ app/models/social_entry_generator.rb
@@ -33,35 +33,7 @@ class SocialEntryGenerator
     target.class.included_modules.include?(RatingMetrizable)
   end
 
-  # def categorize_tags
-  #   return unless @social_entry
-  #   @rating_metrizables ||= []
-  #   @social_entry.tags.select do |tag|
-  #     @rateable ||= tag if rateable?(tag)
-  #     @rateeable ||= tag if rateeable?(tag)
-  #     @raterable ||= tag if raterable?(tag)
-  #     @rating_typeable ||= tag if rating_typeable?(tag)
-  #     @rating_metrizables.push(tag) if rating_metrizable?(tag)
-  #   end
-  # end
-
-  # def all_rating_components_present
-  #   return false if @rateable.nil?
-  #   return false if @rateeable.nil?
-  #   return false if @raterable.nil?
-  #   return false if @rating_typeable.nil?
-  #   true
-  # end
-
   # Generator functions
-
-  # def new_social_entry(params)
-  #   @social_entry = SocialEntry.new(params)
-  #   return @social_entry unless @social_entry.valid?
-  #   @social_entry.create_tags if create_tags
-  #   @social_entry.generate_food_rating if @social_entry.tags.present?
-  #   @social_entry.create_action
-  # end
 
   def create_social_entry(params, create_tags = false)
     @social_entry = SocialEntry.new(params)
